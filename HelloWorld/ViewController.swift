e2ec4c8639dd2ddc61152c434553f5f85f1bba06
andrey <xy1kony4@yandex.ru>
2022-01-22 21:09:29 +0300

    changes in the main screen

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButton: UIButton!
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func makeButtonAction() {
+    }
+    
 }
-
